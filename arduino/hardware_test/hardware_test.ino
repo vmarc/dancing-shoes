900f54d699e60b6c432333f3ce4ca86481474cb9
vmarc <marc@vmarc.be>
2026-05-06 10:09:10 +0200

    arduino hardware test sketch refactored

--- arduino/hardware_test/hardware_test.ino
+++ arduino/hardware_test/hardware_test.ino
@@ -1,45 +1,66 @@
 #include "MultiStepperLite.h"
 
-/******************
-  Configuration
-*******************/
+// Pin Assignments
+constexpr uint8_t PIN_BUTTON_1          = 2;
+constexpr uint8_t PIN_BUTTON_2          = 3;
+constexpr uint8_t PIN_STEPPER_ENABLE    = 6;
+constexpr uint8_t PIN_STEPPER_DIRECTION = 4;
+constexpr uint8_t PIN_STEPPER_PULSE     = 5;
+constexpr uint8_t PIN_SHOE_LEFT         = 7;
+constexpr uint8_t PIN_SHOE_RIGHT        = 8;
 
-#define PIN_BUTTON_1          2 
-#define PIN_BUTTON_2          3
-
-#define PIN_STEPPER_ENABLE    6
-#define PIN_STEPPER_DIRECTION 4
-#define PIN_STEPPER_PULSE     5
-
-#define PIN_SHOE_LEFT         7
-#define PIN_SHOE_RIGHT        8
-
-#define SERIAL_BUFFER_SIZE    256
-
-#define STEPS_PER_REVOLUTION  6400
-
-#define MOTOR_1               0
+// Configuration
+constexpr size_t  SERIAL_BUFFER_SIZE   = 128;
+constexpr int32_t STEPS_PER_REVOLUTION = 6400;
+constexpr uint8_t MOTOR_1              = 0;
 
 MultiStepperLite steppers(1);
 
+// Actions
+void actionLedOn();
+void actionLedOff();
+void actionLeftShoeUp();
+void actionLeftShoeDown();
+void actionRightShoeUp();
+void actionRightShoeDown();
+void actionStepper();
+void actionStepperForward();
+void actionStepperBackward();
+
+struct Action {
+  const char *name;
+  void (*func)();
+};
+
+const Action actions[] = {
+  { "led-on",           actionLedOn           },
+  { "led-off",          actionLedOff          },
+  { "left-shoe-up",     actionLeftShoeUp      },
+  { "left-shoe-down",   actionLeftShoeDown    },
+  { "right-shoe-up",    actionRightShoeUp     },
+  { "right-shoe-down",  actionRightShoeDown   },
+  { "stepper",          actionStepper         },
+  { "stepper-forward",  actionStepperForward  },
+  { "stepper-backward", actionStepperBackward },
+};
+
+constexpr size_t ACTION_COUNT = sizeof(actions) / sizeof(Action);
+
 /******************
-  Setup
+  Core
 *******************/
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB
-  }
+  while (!Serial) {}
+
   setupPins();
   setupSteppers();
 }
 
 void loop() {
   serialTick();
-
-  uint32_t now = micros();
-  steppers.do_tasks(now);
+  steppers.do_tasks(micros());
 }
 
 /******************
@@ -48,59 +69,39 @@ void loop() {
 
 void setupPins() {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);  
+  digitalWrite(LED_BUILTIN, LOW);
 
   pinMode(PIN_BUTTON_1, INPUT_PULLUP);
   pinMode(PIN_BUTTON_2, INPUT_PULLUP);
 
-  pinMode(PIN_STEPPER_ENABLE, OUTPUT);
+  pinMode(PIN_STEPPER_ENABLE,    OUTPUT);
   pinMode(PIN_STEPPER_DIRECTION, OUTPUT);
-  pinMode(PIN_STEPPER_PULSE, OUTPUT);
+  pinMode(PIN_STEPPER_PULSE,     OUTPUT);
 
-  pinMode(PIN_SHOE_LEFT, OUTPUT);
+  pinMode(PIN_SHOE_LEFT,  OUTPUT);
   pinMode(PIN_SHOE_RIGHT, OUTPUT);
-
-  digitalWrite(PIN_SHOE_LEFT, HIGH);  
-  digitalWrite(PIN_SHOE_RIGHT, HIGH);  
+  digitalWrite(PIN_SHOE_LEFT,  HIGH);
+  digitalWrite(PIN_SHOE_RIGHT, HIGH);
 }
 
 void setupSteppers() {
-  digitalWrite(PIN_STEPPER_ENABLE, LOW);  
-  digitalWrite(PIN_STEPPER_DIRECTION, LOW);  
-  steppers.init_stepper(0, PIN_STEPPER_PULSE);
+  digitalWrite(PIN_STEPPER_ENABLE,    LOW);
+  digitalWrite(PIN_STEPPER_DIRECTION, LOW);
+  steppers.init_stepper(MOTOR_1, PIN_STEPPER_PULSE);
 }
 
 /******************
-  Actions
+  Action Handlers
 *******************/
 
-struct Action {
-  char *name;
-  void (*func)(void);
-};
-
-Action actions[] = {
-  {"led-on", actionLedOn},
-  {"led-off", actionLedOff},
-  {"left-shoe-up", actionLeftShoeUp},
-  {"left-shoe-down", actionLeftShoeDown},
-  {"right-shoe-up", actionRightShoeUp},
-  {"right-shoe-down", actionRightShoeDown},
-  {"stepper", actionStepper},
-  {"stepper-forward", actionStepperForward},
-  {"stepper-backward", actionStepperBackward},
-};
-
-size_t actionCount = sizeof(actions) / sizeof(struct Action);
-
-void actionLedOn(void) {
-  digitalWrite(LED_BUILTIN, HIGH);
+void actionLedOn() {
+  digitalWrite(LED_BUILTIN,    HIGH);
   digitalWrite(PIN_SHOE_RIGHT, LOW);
   Serial.println("led-on");
 }
 
-void actionLedOff(void) {
-  digitalWrite(LED_BUILTIN, LOW);
+void actionLedOff() {
+  digitalWrite(LED_BUILTIN,    LOW);
   digitalWrite(PIN_SHOE_RIGHT, HIGH);
   Serial.println("led-off");
 }
@@ -110,33 +111,33 @@ void actionLeftShoeUp() {
   Serial.println("left-shoe-up");
 }
 
-void actionLeftShoeDown(void) {
+void actionLeftShoeDown() {
   digitalWrite(PIN_SHOE_LEFT, LOW);
   Serial.println("left-shoe-down");
 }
 
-void actionRightShoeUp(void) {
+void actionRightShoeUp() {
   digitalWrite(PIN_SHOE_RIGHT, HIGH);
   Serial.println("right-shoe-up");
 }
 
-void actionRightShoeDown(void) {
+void actionRightShoeDown() {
   digitalWrite(PIN_SHOE_RIGHT, LOW);
   Serial.println("right-shoe-down");
 }
 
-void actionStepper(void) {
+void actionStepper() {
   steppers.start_finite(MOTOR_1, 4000, STEPS_PER_REVOLUTION);
   Serial.println("stepper");
 }
 
-void actionStepperForward(void) {
-  digitalWrite(PIN_STEPPER_DIRECTION, LOW);  
+void actionStepperForward() {
+  digitalWrite(PIN_STEPPER_DIRECTION, LOW);
   Serial.println("stepper-forward");
 }
 
-void actionStepperBackward(void) {
-  digitalWrite(PIN_STEPPER_DIRECTION, HIGH);  
+void actionStepperBackward() {
+  digitalWrite(PIN_STEPPER_DIRECTION, HIGH);
   Serial.println("stepper-backward");
 }
 
@@ -144,12 +145,12 @@ void actionStepperBackward(void) {
   Serial
 *******************/
 
-byte bufferIndex = 0;
-char message[SERIAL_BUFFER_SIZE];
-bool messageReady = false;
+static uint8_t bufferIndex = 0;
+static char    message[SERIAL_BUFFER_SIZE];
+static bool    messageReady = false;
 
 void serialTick() {
-  while (Serial.available() > 0 && messageReady == false) {
+  while (Serial.available() > 0 && !messageReady) {
     serialReadMessage();
   }
   if (messageReady) {
@@ -158,34 +159,27 @@ void serialTick() {
 }
 
 void serialReadMessage() {
-  char receivedChar = Serial.read();
-  if (receivedChar != '\n') {
-      message[bufferIndex] = receivedChar;
-      bufferIndex++;
-      if (bufferIndex >= SERIAL_BUFFER_SIZE) {
-          Serial.println("buffer overflow!!!");
-          bufferIndex = SERIAL_BUFFER_SIZE - 1;
-      }
-  }
-  else {
-      message[bufferIndex] = '\0';
-      messageReady = true;
-      bufferIndex = 0;
+  char c = Serial.read();
+  if (c == '\n') {
+    message[bufferIndex] = '\0';
+    messageReady = true;
+    bufferIndex = 0;
+  } else if (bufferIndex < SERIAL_BUFFER_SIZE - 1) {
+    message[bufferIndex++] = c;
+  } else {
+    bufferIndex = 0;
+    Serial.println("buffer overflow!!!");
   }
 }
 
 void serialParseMessage() {
   messageReady = false;
-  boolean actionFound = false;
-
-  for(int i=0; i < actionCount && actionFound == false; i++) {
+  for (size_t i = 0; i < ACTION_COUNT; i++) {
     if (strcmp(actions[i].name, message) == 0) {
-      actionFound = true;
       actions[i].func();
+      return;
     }
   }
-  if (actionFound == false) {
-    Serial.print("unsupported message: ");
-    Serial.println(message);
-  }
+  Serial.print("unsupported message: ");
+  Serial.println(message);
 }
